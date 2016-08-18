59c48a2bec2cb3da34985dc7e2e1ed03420cc818
Manas <mdresswalla@gmail.com>
2016-08-18 09:47:00 +0530

    Error in devicedetailviewcontroller.h

---
+++ MyStore/DeviceDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeviceDetailViewController.h
+//  MyStore
+//
+//  Created by Apple on 12/08/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeviceDetailViewController : UIViewController
+
+@end
---
+++ MyStoreTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyStoreTableViewController.h
+//  MyStore
+//
+//  Created by Apple on 11/08/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyStoreTableViewController : UITableViewController
+
+@end
